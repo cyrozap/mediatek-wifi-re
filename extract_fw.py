15de4b06166d8e7bd55f9b32e9c37cb11bcae564
cyrozap <cyrozap@gmail.com>
2020-05-21 23:38:27 -0500

    Refactor the deobfuscate function and explain the algorithm better
    Hopefully these changes make the function easier to understand.

--- extract_fw.py
+++ extract_fw.py
@@ -111,33 +111,76 @@ def deobfuscate(obfuscated, mode='consec'):
 
     ci_equals_ek_pi = set()
     ek_pi_to_pi = {}
-    for chunk_i in range(1, len(obfs_chunks) - 1):
-        if obfs_chunks[chunk_i] == ek_zero:
-            # The plaintext of the 2nd previous chunk is XORed with the
-            # E_K(P_i) of the previous chunk.
-            plaintext_i2 = xor(obfs_chunks[chunk_i - 1], ek_zero)
-            if chunk_i == 1:
-                # The plaintext XORed with the 0th block of the encrypted data
-                # is actually the IV.
+    chunk_count = len(obfs_chunks)
+    for chunk_i in range(1, chunk_count):
+        second_prev_chunk_i = chunk_i - 2
+        prev_chunk_i = chunk_i - 1
+        curr_chunk_i = chunk_i
+        next_chunk_i = chunk_i + 1
+
+        # Check if the current obfuscated chunk is equivalent to an encrypted
+        # block of 16 null bytes. i.e., C_i == E_K(zeroes)
+        if obfs_chunks[curr_chunk_i] == ek_zero:
+            # It was, so that means that the plaintext of the current chunk is
+            # all zeros.
+            deobfs_chunks[curr_chunk_i] = bytes(bytearray(block_size))
+
+            # Beause a ciphertext block is equal to the XOR of its encripted
+            # plaintext and the plaintext of the block that precedes it, and we
+            # know that the ciphertext of the current block ("C_i") is equal to
+            # a known encrypted block ("E_K(zeroes)"), that means the plaintext
+            # of the previous chunk is all zeros. i.e.,
+            # "C_i = E_K(P_i) XOR P_i_minus_1", and since for the current block
+            # "C_i == E_K(P_i)", that means "C_i = C_i XOR P_i_minus_1", and
+            # after XOR-ing each side by C_i, "zeroes = P_i_minus_1", or
+            # "P_i_minus_1 = zeroes".
+            deobfs_chunks[prev_chunk_i] = bytes(bytearray(block_size))
+
+            # As we've just established, the plaintext of the previous block is
+            # all zeroes ("P_i_minus_1 == zeroes"), and because we know the
+            # encrypted value of a plaintext block of zeroes
+            # ("E_K(P_i_minus_1) == E_K(zeroes)", and "E_K(zeroes)" is known),
+            # we can now solve the equation
+            # "C_i_minus_1 = E_K(P_i_minus_1) XOR P_i_minus_2" for the plaintext
+            # of the second previous block, "P_i_minus_2". To do this, we simply
+            # need to substitute in "E_K(zeroes)" for "E_K(P_i_minus_1)" and
+            # then XOR both sides by "E_K(zeroes)". So now we have
+            # "C_i_minus_1 XOR E_K(zeroes) = P_i_minus_2", or
+            # "P_i_minus_2 = C_i_minus_1 XOR E_K(zeroes)". In other words, the
+            # plaintext of the second previous block is equal to the XOR of the
+            # ciphertext of the previous block and the encrypted block of 16
+            # null bytes.
+            plaintext_i2 = xor(obfs_chunks[prev_chunk_i], ek_zero)
+            if curr_chunk_i == 1:
+                # If the current chunk is 1, the previous chunk is 0, and the
+                # second previous chunk is -1. Chunk -1 doesn't exist in the
+                # obfuscated firmware--rather, the plaintext we calculated for
+                # chunk -1 is the IV that gets XORed with the 0th block of
+                # encrypted data to create the 0th block of obfuscated data, and
+                # it's the same for every firmware for that SoC.
                 print("Found IV: {}".format(plaintext_i2.hex()))
             else:
-                deobfs_chunks[chunk_i - 2] = plaintext_i2
+                deobfs_chunks[second_prev_chunk_i] = plaintext_i2
 
-            # Since we know the plaintext of the 2nd previous chunk, if we
-            # know its C_i is equal to its E_K(P_i), add the C_i/E_K(P_i) to
-            # a lookup table.
-            if (chunk_i - 2) in ci_equals_ek_pi:
-                ek_pi_to_pi[obfs_chunks[chunk_i - 2]] = plaintext_i2
+            # Since we know the plaintext of the 2nd previous chunk, if we know
+            # its ciphertext ("C_i") is equal to its encrypted plaintext
+            # ("E_K(P_i)"), add the C_i/E_K(P_i):P_i mapping to a lookup table
+            # so we can decrypt that E_K(P_i) if we ever see it again.
+            if second_prev_chunk_i in ci_equals_ek_pi:
+                ek_pi_to_pi[obfs_chunks[second_prev_chunk_i]] = plaintext_i2
 
-            # The previous chunk is all zeros.
-            deobfs_chunks[chunk_i - 1] = bytes(bytearray(block_size))
-
-            # The current chunk is all zeros (obviously).
-            deobfs_chunks[chunk_i] = bytes(bytearray(block_size))
-
-            # The next C_i is equal to its E_K(P_i).
-            if deobfs_chunks[chunk_i + 1] != ek_zero:
-                ci_equals_ek_pi.add(chunk_i + 1)
+            # Make sure we're not on the last chunk to avoid indexing errors.
+            if next_chunk_i < chunk_count:
+                # The plaintext of the current chunk is all zeroes
+                # ("P_i == zeroes"), and since
+                # "C_i_plus_1 = E_K(P_i_plus_1) XOR P_i", the next ciphertext
+                # ("C_i_plus_1") is equal to its encrypted plaintext
+                # ("E_K(P_i_plus_1)"), or "C_i_plus_1 == E_K(P_i_plus_1)". This
+                # is useful to know, so we store the index of this chunk in a
+                # set to use in later loop iterations. We skip blocks of
+                # encrypted zeroes because we already handle them separately.
+                if deobfs_chunks[next_chunk_i] != ek_zero:
+                    ci_equals_ek_pi.add(next_chunk_i)
 
     return b''.join(deobfs_chunks)
 
